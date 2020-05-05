4e3b5ab3b0125761cc6c9b4cba99ce25ae47ac74
Patrick Lima <patrickhl94@hotmail.com>
2020-05-05 10:18:26 -0300

    Updated Page Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -30,16 +30,29 @@ interface Balance {
 }
 
 const Dashboard: React.FC = () => {
-  // const [transactions, setTransactions] = useState<Transaction[]>([]);
-  // const [balance, setBalance] = useState<Balance>({} as Balance);
+  const [transactions, setTransactions] = useState<Transaction[]>([]);
+  const [balance, setBalance] = useState<Balance>({} as Balance);
 
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
-      // TODO
+      const response = await api.get('/transactions');
+      console.log(response.data);
+      const { balance: balanc, transactions: transact } = response.data;
+      setTransactions(transact);
+      setBalance(balanc);
     }
 
     loadTransactions();
   }, []);
+
+  function convertValue(value: number): string {
+    const valueConverted = Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+    }).format(Number(value));
+
+    return valueConverted;
+  }
 
   return (
     <>
@@ -51,21 +64,27 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">R$ 5.000,00</h1>
+            <h1 data-testid="balance-income">
+              {convertValue(Number(balance.income) || 0)}
+            </h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">R$ 1.000,00</h1>
+            <h1 data-testid="balance-outcome">
+              {convertValue(Number(balance.outcome) || 0)}
+            </h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">R$ 4000,00</h1>
+            <h1 data-testid="balance-total">
+              {convertValue(Number(balance.total) || 0)}
+            </h1>
           </Card>
         </CardContainer>
 
@@ -81,18 +100,17 @@ const Dashboard: React.FC = () => {
             </thead>
 
             <tbody>
-              <tr>
-                <td className="title">Computer</td>
-                <td className="income">R$ 5.000,00</td>
-                <td>Sell</td>
-                <td>20/04/2020</td>
-              </tr>
-              <tr>
-                <td className="title">Website Hosting</td>
-                <td className="outcome">- R$ 1.000,00</td>
-                <td>Hosting</td>
-                <td>19/04/2020</td>
-              </tr>
+              {transactions.map(transaction => (
+                <tr key={transaction.id}>
+                  <td className="title">{transaction.title}</td>
+                  <td className={transaction.type}>
+                    {transaction.type === 'income' ? '' : '- '}
+                    {convertValue(Number(transaction.value))}
+                  </td>
+                  <td>Sell</td>
+                  <td>20/04/2020</td>
+                </tr>
+              ))}
             </tbody>
           </table>
         </TableContainer>
