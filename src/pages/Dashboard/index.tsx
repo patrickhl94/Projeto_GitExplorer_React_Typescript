5d0f8c8f7ed3ebbf2f026d27f7484991d2ca31e1
Patrick Lima <patrickhl94@hotmail.com>
2020-05-05 22:41:46 -0300

    Updated pages Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,16 +1,15 @@
 import React, { useState, useEffect } from 'react';
-import { format, parseISO } from 'date-fns';
 import { FiChevronDown } from 'react-icons/fi';
+import { format, parseISO } from 'date-fns';
 
 import income from '../../assets/income.svg';
 import outcome from '../../assets/outcome.svg';
 import total from '../../assets/total.svg';
 
 import api from '../../services/api';
+import formatValue from '../../utils/formatValue';
 
 import Header from '../../components/Header';
-
-import formatValue from '../../utils/formatValue';
 
 import { Container, CardContainer, Card, TableContainer } from './styles';
 
@@ -35,31 +34,36 @@ const Dashboard: React.FC = () => {
   const [transactions, setTransactions] = useState<Transaction[]>([]);
   const [balance, setBalance] = useState<Balance>({} as Balance);
 
+  function formatDate(date: string): string {
+    const dateFormatDate = parseISO(date);
+    return format(dateFormatDate, 'dd/MM/yyyy');
+  }
+
   useEffect(() => {
     async function loadTransactions(): Promise<void> {
       const response = await api.get('/transactions');
 
       const { balance: balanc, transactions: transact } = response.data;
-      setBalance(balanc);
-      setTransactions(transact);
+
+      const formatedValuesTransactions = transact.map(
+        (transaction: Transaction) => ({
+          ...transaction,
+          formattedValue: formatValue(transaction.value),
+          formattedDate: formatDate(transaction.created_at.toString()),
+        }),
+      );
+      setTransactions(formatedValuesTransactions);
+
+      const formatedBalance = {
+        income: formatValue(balanc.income),
+        outcome: formatValue(balanc.outcome),
+        total: formatValue(balanc.total),
+      };
+      setBalance(formatedBalance);
     }
 
     loadTransactions();
   }, []);
-
-  function convertValue(value: number): string {
-    const valueConverted = Intl.NumberFormat('pt-BR', {
-      style: 'currency',
-      currency: 'BRL',
-    }).format(Number(value));
-
-    return valueConverted;
-  }
-
-  function formatDate(date: string): string {
-    const dateFormatDate = parseISO(date);
-    return format(dateFormatDate, 'dd/MM/yyyy');
-  }
 
   return (
     <>
@@ -71,27 +75,21 @@ const Dashboard: React.FC = () => {
               <p>Entradas</p>
               <img src={income} alt="Income" />
             </header>
-            <h1 data-testid="balance-income">
-              {convertValue(Number(balance.income) || 0)}
-            </h1>
+            <h1 data-testid="balance-income">{balance.income}</h1>
           </Card>
           <Card>
             <header>
               <p>Saídas</p>
               <img src={outcome} alt="Outcome" />
             </header>
-            <h1 data-testid="balance-outcome">
-              {convertValue(Number(balance.outcome) || 0)}
-            </h1>
+            <h1 data-testid="balance-outcome">{balance.outcome}</h1>
           </Card>
           <Card total>
             <header>
               <p>Total</p>
               <img src={total} alt="Total" />
             </header>
-            <h1 data-testid="balance-total">
-              {convertValue(Number(balance.total) || 0)}
-            </h1>
+            <h1 data-testid="balance-total">{balance.total}</h1>
           </Card>
         </CardContainer>
 
@@ -124,10 +122,10 @@ const Dashboard: React.FC = () => {
                   <td className="title">{transaction.title}</td>
                   <td className={transaction.type}>
                     {transaction.type === 'income' ? '' : '- '}
-                    {convertValue(Number(transaction.value))}
+                    {transaction.formattedValue}
                   </td>
                   <td>{transaction.category.title}</td>
-                  <td>{formatDate(transaction.created_at.toString())}</td>
+                  <td>{transaction.formattedDate}</td>
                 </tr>
               ))}
             </tbody>
